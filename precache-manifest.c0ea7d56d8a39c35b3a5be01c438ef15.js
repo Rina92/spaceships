6d92f109f1f8fbcc45d663e8fe69c9d58d683648
Rina92 <45524237+Rina92@users.noreply.github.com>
2019-10-16 00:54:10 -0400

    Updates

---
+++ precache-manifest.c0ea7d56d8a39c35b3a5be01c438ef15.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32cab4c7ee66cbdde468735ccf90b965",
+    "url": "/spaceships/index.html"
+  },
+  {
+    "revision": "dc0d22021a57c19d4f2a",
+    "url": "/spaceships/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "3874c036c16cc69267e2",
+    "url": "/spaceships/static/css/main.9ed36dc9.chunk.css"
+  },
+  {
+    "revision": "dc0d22021a57c19d4f2a",
+    "url": "/spaceships/static/js/2.e2b4dee2.chunk.js"
+  },
+  {
+    "revision": "3874c036c16cc69267e2",
+    "url": "/spaceships/static/js/main.7d60cf94.chunk.js"
+  },
+  {
+    "revision": "5ec8c8e312e2280082a6",
+    "url": "/spaceships/static/js/runtime-main.8a104aec.js"
+  },
+  {
+    "revision": "2f525e7a4a1ffc94062a1b96467f5239",
+    "url": "/spaceships/static/media/background.2f525e7a.jpg"
+  },
+  {
+    "revision": "9aacd12b6b8eded557f953da12dcca97",
+    "url": "/spaceships/static/media/linkphoto.9aacd12b.svg"
+  },
+  {
+    "revision": "d301b0f29cf0e2bca35ebe703bb2788f",
+    "url": "/spaceships/static/media/refresh.d301b0f2.svg"
+  }
+]);
